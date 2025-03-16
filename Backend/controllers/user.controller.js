f96d6b9650e09afaadc80faad309829e1b67d872
Divya <thakurdivya220703@gmail.com>
2025-03-16 14:04:19 +0530

    Add backend structure with user registration functionality

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,24 @@
+const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
+
+module.exports.registerUser = async (req, res, next) => {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {firstname, lastname, email, password} = req.body;
+    const hashPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hashPassword
+    });
+
+    const token = user.generateAuthToken();
+    res.status(201).json({token, user});
+}
